529587d35971e467d9b6ca5670c6b3d3318beadd
mrlmaia <murilomaia.bb@gmail.com>
2020-03-28 17:34:09 -0300

    route.create() already done

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -5,18 +5,20 @@ const database = require('./database/connection')
 
 const routes = express.Router()
 
+
+
 routes.post('/ongs', async (req, res) => {
-//  const { name, email, whatsapp, cidade, uf } = req.body
+//  const { name, email, whatsapp, city, uf } = req.body
   const data = req.body
 
-  const id = crypto.randomBytes(4).toString('HEX')
-
-  await database.('ongs').insert({
-    id,
+  const ongId = crypto.randomBytes(4).toString('HEX')
+  console.log(data)
+  await database('ongs').insert({
+    ongId,
     ...data
   })
 
-  return res.json({ id })
+  return res.json({ ongId })
 })
 
 module.exports = routes
